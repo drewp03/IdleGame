445ae2fc79095931a79aee801ae721c11265d180
Metroidman97 <chrismetroidman97@gmail.com>
2026-03-13 17:05:03 -0400

    Cleaned up the code a bit
    Cleaned up the code a bit and made room for more modularity

--- Idle Game/Assets/Scripts/Components/Upgrade.cs
+++ Idle Game/Assets/Scripts/Components/Upgrade.cs
@@ -7,21 +7,21 @@ using UnityEngine.UI;
 
 public class Upgrade : MonoBehaviour, IPointerClickHandler
 {
-    public ShellManager shellManager;
-    public float tickTime = 3f;
-    private float timer;
-    private int clickers;
+    public ResourceManager resourceManager;     // The Resource Manager script
+    public float tickTime = 3f;                 // Time it takes for the upgrade to run
+    private float timer;                        // Timer variable
+    private int tier;                           // Number of times the upgrade has been purchased
 
+    // UI and button elements
     public TextMeshProUGUI autoclickerText;
     public Image progressBar;
 
-    public float multiplier;
-    public float paymentPrice;
+    public float multiplier;                    // How much currency the upgrade gives 
+    public int paymentPrice;                    // How much the upgrade costs
 
-    public string upgradeName;
+    public string upgradeName;                  // Upgrade name
 
-    public int numOfUnlockedUpgrades;
-
+    // We probably won't need this enum
     public enum UpgradeState
     {
         Locked,
@@ -31,11 +31,12 @@ public class Upgrade : MonoBehaviour, IPointerClickHandler
 
     void Update()
     {
-        if (clickers > 0)
+        // Makes the upgrade automatically run
+        if (tier > 0)
         {
-            if (timer >= tickTime && clickers > 0)
+            if (timer >= tickTime && tier > 0)
             {
-                shellManager.Shells += clickers * multiplier;
+                resourceManager.Shells += tier * multiplier;    // Will need to modify this to work with any currency type
                 timer = 0f;
             }
             timer += Time.deltaTime;
@@ -43,13 +44,14 @@ public class Upgrade : MonoBehaviour, IPointerClickHandler
         }
     }
 
+    // Activates whenever the upgrade button is pressed
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (shellManager.Shells >= paymentPrice)
+        if (resourceManager.Shells >= paymentPrice)
         {
-            shellManager.Shells -= paymentPrice;
-            clickers++;
-            autoclickerText.text = ("Buy "+upgradeName+" ("+paymentPrice+" Shells)\n" +upgradeName+ ": " + clickers);
+            resourceManager.Shells -= paymentPrice;             // Will need to modify this to work with any currency type
+            tier++;
+            autoclickerText.text = ("Buy "+upgradeName+" ("+paymentPrice+" Shells)\n" +upgradeName+ ": " + tier);   // Displays text on the button. Should probably modify this to show how many currency the upgrade actually gives
         }
     }
 }
