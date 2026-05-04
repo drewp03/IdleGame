02c2ac4cc71b58e9c4ea2f7867daf4d6e8ba4bc7
nominal251 <jackhardy524@gmail.com>
2026-05-04 15:07:20 -0400

    Add cheat button

--- Idle Game/Assets/Scripts/Components/PlusOneButton.cs
+++ Idle Game/Assets/Scripts/Components/PlusOneButton.cs
@@ -7,11 +7,24 @@ public class PlusOneButton : MonoBehaviour, IPointerClickHandler
 {
     public ResourceManager resourceManager;         // Resource Manager script
     public ResourceManager.CurrencyType currency;   // Currency type enum from the Resource Manager script. Currency types can be selected manually in the Unity editor
+    public bool CheatButton = false;
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        // Increments the selected currency when the button is pressed
-        resourceManager.IncrementCurrency(currency);
-        ConsoleManager.toLog = "> +1 Shell";
+        if (!CheatButton)
+        {
+            // Increments the selected currency when the button is pressed
+            resourceManager.IncrementCurrency(currency);
+            ConsoleManager.toLog = "> +1 Shell";
+        }
+        else
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                resourceManager.IncrementCurrency(currency);
+            }
+
+            ConsoleManager.toLog = "> Cheat Button";
+        }
     }
 }
